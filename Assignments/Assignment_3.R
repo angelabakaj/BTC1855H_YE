494f5408d063707da622e4b239705c3efd41ef61
Yemam11 <1youssefemam@gmail.com>
2024-06-26 19:13:53 -0400

    Added functionality to recieve and error handle user input

--- Assignments/Assignment_3.R
+++ Assignments/Assignment_3.R
@@ -1,5 +1,6 @@
 #' Assignment 3
 #' Author: Youssef Emam
+#' To run: ensure that the wordbank txt is in the working directory
 
 
 
@@ -19,14 +20,40 @@ get_words <- function (filepath){
   return(wordsvec)
 }
 
-#TO DO: Create function that error handles user input
-# Include parameter for word or number
-
-
 
 #Read words from file, store as a vector
-#TO DO: Create a function which takes a difficulty and then filters the wordbank before selecting
 wordbank <- get_words("words.txt")
 
 #sample the word
-word <- sample(wordbank,1)
+secretword <- sample(wordbank,1)
+
+#set number of guesses and inform the user how many guesses
+# RULE: User gets n+1 guesses for a word of length n, with a minimum of 5 guesses
+word_len <- nchar(secretword)
+num_guesses <- word_len + 1
+
+#Inform the user of the length of the word, and number of guesses
+cat("The secret word is", word_len, "letters long. You have", num_guesses, "tries to guess the word!\n")
+
+
+#Create a vector the same size as the word with "_"s to represent missing letters
+# We are going to replace the "_"s with the letters they guess correctly
+
+progress <- 1:word_len
+
+for (i in progress){
+  progress[i] <- "_"
+}
+
+for (i in num_guesses){
+  # Show progress
+  cat(progress)
+  
+  #Take a guess from the user
+  
+  #determine if the guess is correct
+  
+  #Exit if correct
+}
+
+
