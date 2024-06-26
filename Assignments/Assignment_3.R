3f11dd8cfcfc8d66c1aa90e81df61edc13eb2bd4
Yemam11 <1youssefemam@gmail.com>
2024-06-26 00:53:01 -0400

    Added function which takes the filepath for a wordbank.txt and reads it into R as a vector

--- Assignments/Assignment_3.R
+++ Assignments/Assignment_3.R
@@ -2,3 +2,13 @@
 #' Author: Youssef Emam
 
 
+
+# Defining a function which reads words from a .txt into a vector
+# The words used in my file were taken from: https://github.com/Xethron/Hangman/blob/master/words.txt
+#' @name get_words
+#' @param filepath
+#' @output Vector containing words from the file
+get_words <- function (filepath){
+  words <- read.table(file = filepath, header = F, strip.white = T, col.names = "Words")
+  return(words)
+}
