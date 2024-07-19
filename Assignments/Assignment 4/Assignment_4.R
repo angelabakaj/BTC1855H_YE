8319e2fc178b26a5ec8cad3a1da95502eb8cef62
Yemam11 <1youssefemam@gmail.com>
2024-07-18 20:32:13 -0400

    Cleaned the country column

--- Assignments/Assignment 4/Assignment_4.R
+++ Assignments/Assignment 4/Assignment_4.R
@@ -2,3 +2,7 @@
 #' Author: Youssef Emam
 #' Date: July 19th 2019
 #' Notes: To run this program ensure that the CSV is in the working directory
+
+
+#read the file into the environment
+martians <- read.csv("ufo_subset.csv", header = T)
